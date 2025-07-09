66a39f8b035257aafa4511c8e34edd17125c3663
Fahad2007raja <rajamfahadidrees07@gmail.com>
2025-07-09 13:57:31 +0500

    Checking number is even or odd using if else

---
+++ if_else_concept_in_numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int  num1,num2;
+    cout<<"Enter your number :"<<endl;
+    cin>>num1>>num2;
+    if (num1%num2)
+
+    {
+    cout<<"Your number is even"<<endl;
+    }
+    else{
+        cout<<"Your number is odd"<<endl;
+    }
+    return 0;
+}
