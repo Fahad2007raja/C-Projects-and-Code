19b0cab77f7738bc137b6876bbb86bfcba111655
Fahad2007raja <rajamfahadidrees07@gmail.com>
2025-07-09 18:18:15 +0500

    finding area of triangle

---
+++ Area_of_Triangle.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    float area,b,h;
+    cout<<"Enter value of b :"<<endl;
+    cin>>b;
+    cout<<"Enter value of h :"<<endl;
+    cin>>h;
+    
+    area=0.5*b*h;
+    cout<<"Area: "<<area;
+    return 0;
+    
+}
