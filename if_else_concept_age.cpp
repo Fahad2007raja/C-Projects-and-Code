18477990d3d1cd16b24dcd821b7120c187c9519f
Fahad2007raja <rajamfahadidrees07@gmail.com>
2025-07-09 13:45:34 +0500

    Understanding the concept of if else in age

---
+++ if_else_concept_age.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    cout<<"Enter your age :"<<endl;
+    cin>>age;
+    if( age>=18 )
+    {cout<<"You can cast vote"<<endl;
+    
+}
+else{
+    cout<<"You cannot cast vote"<<endl;
+}
+return 0;
+}
